fee8826b382dbbebb89edd939186fc2c448cef93
mhweaver <mhweaver@gmail.com>
2016-11-29 10:29:21 -0700

    Added somewhat useful comment

--- src/Segment.hs
+++ src/Segment.hs
@@ -11,7 +11,7 @@ import Control.Concurrent.STM
 import Control.Concurrent
 import Data.Text
 
-newtype SegmentOutput = SegmentOutput (TMVar (), TVar Text)
+newtype SegmentOutput = SegmentOutput (TMVar (), TVar Text) -- (Update notifier, Output)
 data Segment = Segment { runSegment :: IO ()
                        , getOutput :: SegmentOutput
                        }
